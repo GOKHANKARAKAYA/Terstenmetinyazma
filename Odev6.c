7c94b9d93aaada8d7ef7ccb9f9105d494f8f5ef1
Gökhan <126273014+GOKHANKARAKAYA@users.noreply.github.com>
2023-06-18 14:17:47 +0300

    Odev6.c

--- Odev6.c
+++ Odev6.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+int main(){
+    float sayi;
+    
+    printf("Bir sayi giriniz: ");
+    scanf("%f",&sayi);
+    
+    if(sayi - (int)sayi == 0){
+        printf("\nGirdiginiz sayi ondalikli bir sayi degildir.");
+    }
+    else{
+        printf("Girdiginiz sayi ondalikli bir sayidir.");
+    }
+}
