bae32f40b44eead63b80b65e90355e8f625cc1ae
Gökhan <126273014+GOKHANKARAKAYA@users.noreply.github.com>
2023-06-18 14:17:01 +0300

    Update Odev5.c

--- Odev5.c
+++ Odev5.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+main()
+{
+    int faktoriyel = 1; 
+    int sayi;
+    
+    printf("Bir sayi giriniz: ");
+    scanf("%d",&sayi);
+    
+    for(int i = 1; i <= sayi; i++)
+    {
+        faktoriyel *= i;                                      
+    }
+    printf("%d != %d",sayi, faktoriyel);
+}
